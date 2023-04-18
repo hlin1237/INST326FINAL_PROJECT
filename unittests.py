c7fc4f5e0f8ba6128286a564c8879fe2167ea41b
msahib1 <98369937+msahib1@users.noreply.github.com>
2023-04-17 22:01:40 -0400

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -1,3 +1,10 @@
 import unittest
 from tic_tac_toe import *
 
+class TestTicTacToe(unittest.TestCase):
+    def test_display_board(self):
+        board = [['X', 'O', 'X'], ['O', 'X', 'O'], ['X', 'O', 'X']]
+        expected_output = "X|O|X\nO|X|O\nX|O|X\n"
+        self.assertEqual(display_board(board), expected_output)
+        
+
