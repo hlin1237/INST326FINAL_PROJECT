2b33188532874f633de7a0299340223ca89b9868
hlin1237 <129188782+hlin1237@users.noreply.github.com>
2023-05-09 15:39:10 -0400

    Update unittests.py

--- unittests.py
+++ unittests.py
@@ -2,25 +2,23 @@ import unittest
 from tic_tac_toe import *
 
 class TestTicTacToe(unittest.TestCase):
-    def test_display_board(self):
+    def test_next_turn(self):
         pass
     
-    def test_get_player_move(self):
+    def test_check_winner(self):
         pass 
-    
-    def test_get_computer_move(self):
-        pass 
-    
-    def test_check_win(self):
-        pass
     
     def test_check_tie(self):
+        pass 
     
-    def test_reset_board(self):
+    def test_new_game(self):
         pass
     
-    def test_play_again(self):
+    def test_ai_play(self):
+    
+    def test_make_buttons(self):
         pass
+
 
 
 
