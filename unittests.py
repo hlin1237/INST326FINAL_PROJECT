dc03ee1453b73c303b0e2598ffe2c85b886b3e7b
msahib1 <98369937+msahib1@users.noreply.github.com>
2023-05-13 19:50:11 -0400

    c

--- unittests.py
+++ unittests.py
@@ -3,7 +3,14 @@ from tic_tac_toe import *
 
 class TestTicTacToe(unittest.TestCase):
     def test_next_turn(self):
-        pass
+        global buttons, players, player
+        buttons = [[{"text": ""} for j in range(3)] for i in range(3)]
+        players = ["X", "O"]
+        player = players[0]
+
+        next_turn(0, 0)
+
+        self.assertEqual(buttons[0][0]["text"], players[0])
     
     def test_check_winner(self):
         pass 
